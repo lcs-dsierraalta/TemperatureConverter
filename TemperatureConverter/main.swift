258148d64fa088ee220fc7f0be31eb6d1bf0f40e
Diego Sierraalta <dsierraalta@lcs.on.ca>
2021-02-02 15:09:02 -0500

    Made basic UI with choices of which temperature the user wants to convert from

--- TemperatureConverter/main.swift
+++ TemperatureConverter/main.swift
@@ -9,3 +9,13 @@ import Foundation
 
 print("Hello, World!")
 
+//MARK: Input
+
+//Show menu and choices
+print("Hi, I am a temperature converter")
+print("What temperature do you want to convert from?")
+print("")
+print("1 - Celcius")
+print("2 - Farenheit")
+print("3 - Kelvin")
+print("")
