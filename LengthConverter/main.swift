c90a535bac042a9b1a32f23f138a648abe23937e
Diego Sierraalta <dsierraalta@lcs.on.ca>
2021-02-03 21:33:46 -0500

    Added a function for converting form a meters to a numbers of different lengths

--- LengthConverter/main.swift
+++ LengthConverter/main.swift
@@ -11,30 +11,61 @@ func convertToMeter(fromBase base: String, length: Double) -> Double {
     var meterEquivalent = 0.0
     switch meterEquivalent {
     case "1":
-        meterEquivalent = length / 1000
+        meterEquivalent = length * 1000
     case "2":
         meterEquivalent = length
     case "3":
-        meterEquivalent = length * 100
+        meterEquivalent = length / 100
     case "4":
-        meterEquivalent = length * 1000
+        meterEquivalent = length / 1000
     case "5":
-        meterEquivalent = length * 1_000_000
+        meterEquivalent = length / 1_000_000
     case "6":
-        meterEquivalent = length * 1_000_000_000
+        meterEquivalent = length / 1_000_000_000
     case "7":
-        meterEquivalent = length * 1609
+        meterEquivalent = length / 1609
     case "8":
-        meterEquivalent = length * 1.09361
+        meterEquivalent = length / 1.09361
     case "9":
-        meterEquivalent = length * 3.28084
+        meterEquivalent = length / 3.28084
     case "10":
-        meterEquivalent = length * 39.3701
+        meterEquivalent = length / 39.3701
     default:
         break
     }
     return meterEquivalent
 }
+
+
+func lengthConversion(toBase base: String, length: Double) -> Double {
+    var convertedValue
+    switch convertedValue {
+    case "1":
+        convertedValue = length / 1000
+    case "2":
+        convertedValue = length
+    case "3":
+        convertedValue = length * 100
+    case "4":
+        convertedValue = length * 1000
+    case "5":
+        convertedValue = length * 1_000_000
+    case "6":
+        convertedValue = length * 1_000_000_000
+    case "7":
+        convertedValue = length * 1609
+    case "8":
+        convertedValue = length * 1.09361
+    case "9":
+        convertedValue = length * 3.28084
+    case "10":
+        convertedValue = length * 39.3701
+    default:
+        break
+    }
+    return convertedValue
+}
+
 
 //MARK: Input
 print("Length Converter")
